342608985e7ce555d992d029272692660a1ef73d
Bansilal Keloth <109201538+bansilal8800@users.noreply.github.com>
2022-07-13 15:10:33 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,5 +1,7 @@
 
 import streamlit
+import pandas
 streamlit.title('My Parents needs Healthy Dinner')
-
 streamlit.title('This is my first application with GITHUB and Streamlit')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
